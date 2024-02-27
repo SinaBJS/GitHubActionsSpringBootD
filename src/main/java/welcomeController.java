d42d4549b8db6aadaef9cb8dd5cacab932c1cef8
moha3052 <moha3052@hotmail.dk>
2024-02-27 13:37:49 +0100

    13:37

---
+++ src/main/java/welcomeController.java
@@ -0,0 +1,11 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class welcomeController {
+
+    @GetMapping()
+    public String index(){
+        return "Welcome";
+    }
+}
